04a360e57e765b4d0d98a4124378c1be4754b1ed
gotonis <guertin.person@gmail.com>
2021-02-23 20:49:55 -0500

    added functionality for Ulam-like triangle spiral
    no automatic plotting yet, but optional arguments allow for
    spirals derived from arbitrary triangles.
    Arguments ask for 3 2d vectors; if these add to [0,0] a spiral will be made
    whose boundary approaches the triangle made by stacking those tip-to-tail.
    
    The math for plotting assumes these vectors add to [0,0]. Behavior has not
    been explored where this is not the case.
    
    TODO: add better documentation, comment out debug prints, add auto plotting
    
    Next: regular n-gon
    
    I wanted to eventually allow the user to specify a more arbitrary pattern
    of when to extend the segment and how far to rotate for the next segment,
    but I haven't yet figured out an efficient way to calculate the position of
    a point without iterating through the spiral up to that point, and I think I'd
    rather focus on polygons and add features for users to play around with those.

--- Ulam.py
+++ Ulam.py
@@ -50,11 +50,41 @@ def spiral(n):
             return [round(-k+(m-n),2),k]
         return [k,round(k-(m-n-t),2)]
 
-def plotSpiral(func,x,origin=1):
+def plotSpiral(func,x,origin=1, **kwargs):
     fig,ax = plt.subplots()
     for i in x:
-        if i > origin and func(i) > origin:
+        if i >= origin and func(i) >= origin:
             t = np.array([spiral(i-origin+1),spiral(func(i)-origin+1)])
-            ax.plot(t[:,0],t[:,1])
+            ax.plot(t[:,0],t[:,1],**kwargs)
+            ax.scatter(t[:,0],t[:,1])
+            #ax.grid(1, which='both',markevery=1);
     fig
+
+def triSpiral(n, a=np.array([1.0,0]),b=np.array([np.cos(2*np.pi/3),np.sin(2*np.pi/3)]),c=np.array([np.cos(4*np.pi/3),np.sin(4*np.pi/3)])):
+    if n <= 0:
+        return np.array([0,0])
+    if n <= 1:
+        return np.array([n,0])
     
+    k = np.ceil((np.sqrt(8*n+1)-1)/6)-1
+    print("{} is in layer {}".format(n,k))
+    vertex = [0,0]
+    dt = [0,0]
+    if n <= (9*k**2 + 9*k+2)/2:
+        vertex = k*(c-a)
+        t = (n - (9*k**2 + 3*k)/2)
+        dt = t*a
+        print("{} units along segment 1".format(t))
+    elif n <= (9*k**2 + 15*k)/2 + 3:
+        vertex = k*(a-b)+a
+        t = (n - (9*k**2 + 9*k + 2)/2)
+        dt = t*b
+        print("{} units along segment 2".format(t))
+    else:
+        vertex = k*(b-c)+a+2*b
+        t = (n- (9*k**2 + 15*k + 6)/2)
+        dt = t*c
+        print("{} units along segment 3".format(t))
+    return vertex+dt
+
+        
